92dc6590faee81e6bcc25e45903edb06db998e52
unknown <samantha.c.egge@gmail.com>
2019-06-14 09:39:44 -0700

    Added a button to remove a smurf item.

--- village/src/App.js
+++ village/src/App.js
@@ -43,6 +43,15 @@ class App extends Component {
       })
   }
 
+  removeSmurf = (event, smurfId) => {
+    event.preventDefault();
+    console.log(smurfId);
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurfId}`)
+      .then(response => {this.setState ({ smurfs: response.data }) })
+      .catch(error => console.error('Server Error', error))
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -53,12 +62,10 @@ class App extends Component {
         <NavLink to='/smurf-form'>Add a Smurf</NavLink>
         <Route exact path='/' 
           render={(props) => 
-          <Smurfs {...props} smurfs={this.state.smurfs} />} />
+          <Smurfs {...props} smurfs={this.state.smurfs} removeSmurf={this.removeSmurf} />} />
         <Route exact path='/smurf-form' 
           render={(props) => 
           <SmurfForm addSmurf={this.state.Smurfs} />} />
-        {/* <SmurfForm addSmurf={this.addSmurf} />
-        <Smurfs smurfs={this.state.smurfs} /> */}
       </div>
     );
   }
